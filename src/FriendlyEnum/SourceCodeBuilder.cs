146b6f9c06703800da1e3e9aefec3c679f58b785
dushegubka <andrey@andreev.ru>
2024-03-12 10:34:50 +0300

    Добавил SourceCodeBuilder.cs и SyntaxExtensions.cs

---
+++ src/FriendlyEnum/SourceCodeBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace FriendlyEnum;
+
+public static class SourceCodeBuilder
+{
+    public static SourceText BuildExtensionClassSourceCode(
+        string? extensionClassName,
+        Dictionary<EnumDeclarationSyntax, List<EnumMemberDeclarationSyntax>> enums)
+    {
+        var builder = new StringBuilder();
+
+        builder.AppendLine($"public static class {extensionClassName}");
+        builder.AppendLine("{");
+        
+        foreach (var item in enums)
+        {
+            builder.AppendLine(BuildExtensionMethodSourceCode(item.Key, item.Value));
+        }
+        
+        builder.AppendLine("}");
+        
+        return SourceText.From(builder.ToString(), Encoding.UTF8);
+    }
+
+    private static string BuildExtensionMethodSourceCode(
+        EnumDeclarationSyntax enumDeclaration,
+        List<EnumMemberDeclarationSyntax> enumMembers)
+    {
+        var builder = new StringBuilder();
+        var enumName = enumDeclaration.Identifier.Text;
+        
+        builder.AppendLine($"public static string ToFriendlyName(this {enumName} value)");
+        builder.AppendLine("{");
+        builder.AppendLine("return value switch");
+        builder.AppendLine("{");
+        
+        foreach (var enumMember in enumMembers)
+        {
+            var currentMemberName = enumMember.Identifier.Text;
+            var friendlyName = enumMember.GetFriendlyNameAttributeValue();
+            
+            builder.AppendLine(string.IsNullOrEmpty(friendlyName)
+                // Example: Enum.One => nameof(Enum.One)
+                ? $"{enumName}.{currentMemberName} => nameof({enumName}.{currentMemberName}),"
+                // Example: Enum.One => "First"
+                : $"{enumName}.{currentMemberName} => {friendlyName},");
+        }
+        
+        builder.AppendLine("_ => throw new ArgumentException(nameof(value))");
+        builder.AppendLine("};");
+        builder.AppendLine("}");
+        
+        return builder.ToString();
+    }
+}
---
+++ src/FriendlyEnum/SyntaxExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Text;
+
+namespace FriendlyEnum;
+
+public static class SyntaxExtensions
+{
+    public static string? GetFriendlyNameAttributeValue(this EnumMemberDeclarationSyntax enumMember)
+    {
+        return enumMember
+            .AttributeLists
+            .SelectMany(GetAttributes)
+            .FirstOrDefault(IsFriendlyNameAttribute)
+            ?.ArgumentList?.Arguments
+            .Select(GetExpressionText)
+            .FirstOrDefault();
+    }
+
+    public static SourceText ReformatAndCleanup(this SourceText sourceText)
+    {
+        var tree = CSharpSyntaxTree.ParseText(sourceText.ToString());
+        var root = tree.GetRoot();
+
+        var workspace = new AdhocWorkspace();
+        var options = workspace.Options;
+        options = options.WithChangedOption(FormattingOptions.UseTabs, LanguageNames.CSharp, false);
+        options = options.WithChangedOption(FormattingOptions.TabSize, LanguageNames.CSharp, 4);
+
+        var formattedNode = Formatter.Format(root, workspace, options);
+
+        return SourceText.From(formattedNode.ToFullString(), Encoding.UTF8);
+    }
+    
+
+    private static IEnumerable<AttributeSyntax> GetAttributes(AttributeListSyntax attributeList)
+    {
+        return attributeList.Attributes;
+    }
+
+    private static bool IsFriendlyNameAttribute(AttributeSyntax attribute)
+    {
+        return attribute.Name.ToString() == "FriendlyName";
+    }
+
+    private static string GetExpressionText(AttributeArgumentSyntax argument)
+    {
+        return argument.Expression.ToString();
+    }
+}
