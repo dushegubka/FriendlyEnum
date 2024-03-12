edfec04b13a9393ea780b2e14460e143041a7177
dushegubka <andrey@andreev.ru>
2024-03-12 10:33:16 +0300

    Добавил FriendlyEnumSyntaxReceiver

---
+++ src/FriendlyEnum/FriendlyEnumSyntaxReceiver.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace FriendlyEnum;
+
+public class FriendlyEnumSyntaxReceiver : ISyntaxReceiver
+{
+    public Dictionary<EnumDeclarationSyntax, List<EnumMemberDeclarationSyntax>> Enums { get; } = new();
+
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        if (syntaxNode is not EnumDeclarationSyntax enumDeclaration)
+            return;
+        
+        if (!HasFriendlyEnumAttribute(enumDeclaration))
+            return;
+        
+        Enums.Add(enumDeclaration, enumDeclaration.Members.ToList());
+    }
+
+    private bool HasFriendlyEnumAttribute(EnumDeclarationSyntax syntax)
+    {
+        return syntax.AttributeLists
+            .SelectMany(x => x.Attributes)
+            .Any(x => x.Name.ToString() == "FriendlyEnum");
+    }
+}
